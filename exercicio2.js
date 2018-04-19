b0b2263c84d2e66c0bf89207dc5b9891a6c2169a
Victor Balbo <victorbalbo@gmail.com>
2018-04-18 22:24:42 -0300

    Final commit (works alot)

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,21 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let currentImg = 0
+document.querySelectorAll('.controle').forEach((button) => {
+  button.addEventListener('click', (event) => {
+    if(event.target.id === 'proximo') {
+      currentImg++
+      if(currentImg >= todasAsImagens.length) {
+        currentImg = 0
+      }
+    } else {
+      currentImg--
+      if(currentImg < 0) {
+        currentImg = todasAsImagens.length - 1
+      }
+    }
+    document.querySelector('#slide').src = servidorDasImagens + todasAsImagens[currentImg]
+  })
+})
